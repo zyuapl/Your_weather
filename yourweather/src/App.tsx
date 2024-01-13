edac9c2f0f3f0aaf8c2aebc1dd511bf89d784b8b
zyuapl <mczyua@gmail.com>
2024-01-13 01:14:57 +0100

    modified:   yourweather/src/App.tsx 	modified:   yourweather/src/Router.tsx 	renamed:    yourweather/src/WeatherList.tsx/SingleWeather.tsx -> yourweather/src/WeatherList/SingleWeather.tsx 	renamed:    yourweather/src/WeatherList.tsx/WeatherList.tsx -> yourweather/src/WeatherList/WeatherList.tsx 	renamed:    yourweather/src/WeatherList.tsx/WeatherLocation.tsx -> yourweather/src/WeatherList/WeatherLocation.tsx

--- yourweather/src/App.tsx
+++ yourweather/src/App.tsx
@@ -1,6 +1,6 @@
 import "./style.scss"
-import SingleWeather from "./WeatherList.tsx/SingleWeather";
-import WeatherList from "./WeatherList.tsx/WeatherList";
+import SingleWeather from "./WeatherList/SingleWeather";
+import WeatherList from "./WeatherList/WeatherList";
 import { Outlet} from "react-router-dom";
 
 export default function App() {
--- yourweather/src/Router.tsx
+++ yourweather/src/Router.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import WeatherList from "./WeatherList.tsx/WeatherList";
-import SingleWeather from "./WeatherList.tsx/SingleWeather";
+import WeatherList from "./WeatherList/WeatherList";
+import SingleWeather from "./WeatherList/SingleWeather";
 
 export default function Router() {
     return <RouterProvider router={router} />;
